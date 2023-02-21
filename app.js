c8fefae1058ca2232b84ceeab44e2f71e6793478
Syed Muhammad Mustafa Zaidi <121306109+syedmuhammadmustafazaidi@users.noreply.github.com>
2023-02-21 21:09:39 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,390 @@
+// Chapter 1 (Alert)
+
+// Question 1
+// alert('Welcom To My Page')
+
+// Question 2
+// alert('Error! Please enter a valid password')
+
+// Question 3
+// alert('Welcome to JS Land...\nHappy Coding!')
+
+// Question 4
+// alert("Welcome to JS Land...")
+// alert("Happy Coding!")
+
+// Question 5
+// alert("Hello... I can run JS through my web browser's consle")
+// console.log("Hello... I can run JS through my web browsers's consle")
+
+// Chapter 2
+
+// Question 1
+// var username
+
+// Question 2
+// var myName = "Arsalan Ahmed"
+
+// Question 3
+// var JS = "Hello World"
+// alert(JS)
+
+// Question 4
+// var myName = "Ali Khan"
+// var age = "17 years old"
+// var uni = "SSUET"
+// var subject = "Mobile & Cloud Computing - Module A"
+// alert(myName)
+// alert(age)
+// alert(uni)
+// alert(subject)
+
+// Question 5
+// var pizza = ("PIZZA\nPIZZ\nPIZ\nPI\nP")
+// alert(pizza)
+
+// Chapter 3
+
+// Question 1
+// var age = "I am 14 years old"
+// alert(age)
+
+// Question 2
+// var count = 21
+// alert("You have visited this site" +" "+ count + " " + "nums")
+
+// Question 3
+// var birthYear = 1990;
+// document.write("My birth year is " + birthYear);
+
+// Question 4
+// var visitorName = "John Doe"
+// var productTitle = "M&S"
+// var quantity = 5
+// document.write(visitorName + " ordered " + quantity + "&nbsp" + productTitle + "&nbsp" + "T-Shirt(s) on XYZ Clothing store.")
+
+// Chapter 4
+
+// Question 1
+// var num = 1 var num1 = 2 var num2 = 3
+
+// Question 2
+
+// Legal Variables
+
+// var fname = "Arsalan"
+// var lname = "Ahmed"
+// var city = "Karachi"
+// var address = "Lyari"
+// var contactNo = 0320-8374982
+
+// Illegal Variables
+
+// var nme = "Arsalan"
+// var lnme = "Ahmed"
+// var cty = "Karachi"
+// var addr = "Lyari"
+// var cnno = 0320-8374982
+
+// Question 3
+// document.write("a. A heading stating 'Rules for naming JS variables'<br>"+
+// "b. Variable names can only contain _________, __________,__________ and __________.<br>"+
+// "For example $my_1stVariable <br>"+
+// "c. Variable must begin with a _________,__________ or _________. For example $name,_name or name <br>"+
+// "d. Variable names are case ______________"+
+// "e. Variable names should not be JS ______________")
+
+// Chapter 5 to 7
+
+// Question 1
+// var total = 13 + 9
+// alert("Sum of 13 and 9 is " + total)
+
+// Question 2
+
+// Subtraction
+// var total = 13 - 9
+// alert("Result of subtracting 9 from 13 is " + total)
+
+// Multiplication
+// var total = 13 * 9
+// alert("Result of multiplying 13 and 9 is " + total)
+
+// Divide
+// var total = 13 / 9
+// alert("Result of dividing 13 by 9 is " + total)
+
+// Modulus
+// var total = 13 % 9
+// alert("Remainder of dividing 13 by 9 is " + total)
+
+// Question 3
+// var num;
+// document.write("Value after variable declaration is" + "&nbsp" + num + "<br>")
+// num = 3;
+// document.write("Initial value is " + num + "<br>" + 
+// "Value after increment is "+ ++num + "<br>" +
+// "Value after addition is "+ (num + 7) + "<br>" +
+// "Value after decrement is "+ (num + 9 - --num) + "<br>" +
+// "The remainder is " + num /3)
+
+// Question 4
+// var ticket = 600
+// document.write("Total cost to buy 5 tickets to a movie is " + (ticket * 5) + " PKR")
+
+// Question 5
+// document.write("Multiplication table of 4"+"<br>")
+
+// var i = +prompt("enter a number for table" , 4)
+// var j = +prompt("enter a number for table start")
+// var k = +prompt("enter a number for table end")
+
+// do{
+//     document.write(i + "x" + j + "=" + j*i +"<br>");
+//     j++;
+// }
+
+// while(j<=k);
+
+// Chapter 8 to 11
+
+// Question 1
+// var userName = prompt("Please enter your name")
+// alert("Hi " + userName)
+
+// Question 2
+// document.write("Multiplication of table 7" + "<br>")
+// var i = +prompt("Enter a table number" , "5")
+// var j = +prompt("Enter a number from table start")
+// var k = +prompt("Enter the ending number")
+
+// do{
+//     document.write(i + "x" + j + "=" + i*j + "<br>");
+//     j++;
+// }
+
+// while(j<=k);
+
+// Question 3
+// var city = prompt("Please enter your city")
+//     if (city == "Karachi") {
+//         alert("Welcome to the city of lights")
+//     } else {
+//     alert("City not found")
+//     }
+
+// Question 4
+// var gender = prompt("Enter your gender")
+// if (gender == "Male") {
+//     alert("Good Morning Sir.")
+// } 
+// else if (gender == "Female") {
+//     alert("Good Morning Ma'am")
+// }
+// else{
+//     alert("gender not found")
+// }
+
+// Question 5
+// var traffic = prompt("Enter color of road traffic signal")
+// if (traffic == "red") {
+//     alert("Vehicle must stop")
+// } 
+
+// else if (traffic == "yellow") {
+//     alert("Vehicle should get ready to move")
+// }
+
+// else if (traffic == "green") {
+//     alert("Vehicle can move now")
+// }
+
+// else {
+//     alert("Color not found")
+// }
+
+// Chapter 12 to 13
+
+// Question 3
+// var num = +prompt("Enter your age")
+// if (num >= 18) {
+//     alert("Old Enough")
+// } else {
+//     alert("Too young")
+// }
+
+// Question 4
+// var fname = prompt("Enter Name 'Miz' in below filed.")
+// if (fname === "Miz") {
+//     document.write("Thank you for visiting Miz")
+// }
+
+// Question 8
+// var num = +prompt("Enter time like: 1900")
+// if (num>=0000 && num<1200) {
+//     alert("Good Morning")
+// } else if (num>=1200 && num <1700) {
+//     alert("Good Afternoon")
+// } else if (num>=1700 && num<2100) {
+//     alert("Good Evening")
+// } else if (num>=2100 && num<=2359) {
+//     alert("Good Night")
+// } else {
+//     alert("Please enter correct value")
+// }
+
+// Question 11
+// var fname = +prompt("Script to display 'You are not Fahad'")
+// var firstName = "Ali"
+// if (firstName === "Fahad") {
+//     document.write("Hello Fahad!")
+// } else {
+//     document.write("You are not Fahad")
+// }
+
+// Question 12
+// var greeting;
+// var hour = 13
+// if (hour<18) {
+//     document.write(greeting = "Good Day")
+// } else {
+//     document.write(greeting = "Good Evening")
+// }
+
+// Chapter 14 to 17
+
+// Question 8
+// var arr = ["SSC","HSC","BCS","BS","BCOM","MS","M.Phil","PhD"]
+// for(i=0; i<arr.length; i++){
+//     document.write(i + " " + arr[i] + "<br>")
+// }
+
+// Question 9
+// var arr;
+// arr = ["Avengers: Age of Ultron"]
+// var arr2 = arr.push("Spectre")
+// var arr3 = arr.push("Jurassic Worldc")
+// var arr4 = arr.push("Inside Out")
+// for(i=0; i<arr.length; i++){
+//     document.write(i + " " + arr[i] + "<br>")
+// }
+// document.write("<br>Total length of arry is " + arr.length)
+// console.log(arr)
+
+// Question 10
+// var arr = ["Audi","Volvo","Ford","Lamborghini"]
+// document.write(arr +"<br><br>")
+// document.write("First index of the array: "+ arr.indexOf("Audi") + "<br>" )
+// document.write("Car at first index of the array: " + arr[0] + "<br>")
+// document.write("First index of the array: "+ arr.lastIndexOf("Lamborghini") + "<br>" )
+// document.write("Car at first index of the array: " + arr[3] + "<br>")
+
+// Question 11
+// var studentName = ["Michael","John","Tony"]
+// var scoresOfStudent = [320,230,480]
+// for(i=0; i<studentName.length; i++){
+//     for(j=i; j<=i; j++){
+//         document.write("Score of " + studentName[i] + " " + "is " + scoresOfStudent[j] + "." + " Percentage: " + (scoresOfStudent[j]/500)*100 + "% <br>")
+//     }
+// }
+
+// Question 13
+// var arr = [320,230,480,120]
+// document.write("Scores of Students: " + arr +"<br><br>")
+// var arr2 = arr.sort()
+// document.write("Ordered Scores of Students " + arr2)
+
+// Chapter 18 to 20
+
+// Question 1
+// var msg = "Hello World"
+// for(i=1; i<6; i++){
+//     document.write(msg+"<br>")
+// }
+
+// Question 2
+// for(i=1; i<11; i++){
+//     document.write(i+"<br>")
+// }
+
+// Question 3
+// var tableNumber = +prompt("Enter table number")
+// var tableRange = +prompt("Enter table range")
+// for(i=1; i<=tableRange; i++){
+//     document.write(tableNumber + " x " + i + " = " + tableNumber*i + "<br>")
+// }
+
+// Question 4
+// var arr = ["Nokia","Samsung","Apple","Sony","Huawei"]
+// for(i=0; i<arr.length; i++){
+//     document.write(arr[i]+"<br>")
+// }
+
+// Question 5
+// var arr = ["apple","banana","mango","orange","strawberry"]
+// for(i=0; i<arr.length; i++){
+//     document.write(arr[i]+"<br>")
+//     // document.write("Element at index"+arr[i].indexOf()+ " is " + arr[i])
+// }
+
+// for(i=0; i<arr.length; i++){
+//     document.write("Element at index "+arr.indexOf(arr[i])+ " is " + arr[i] + "<br>")
+// }
+
+// Chapter 20 to 25
+
+// Question 1
+// var fName = prompt("Enter your first Name")
+// var lName = prompt("Enter you last Name")
+// var concate = fName + " " + lName
+// alert("Welcome to the page " + concate)
+
+// Question 2
+// var userInput = prompt("Enter your favourite mobile phone model")
+// document.write("My favourite phone is: "+userInput)
+// document.write("<br><br> Lenght of String: "+userInput.length)
+
+// Question 3
+// var word = "Pakistani"
+// document.write("Index of 'n': " + word.indexOf("n"))
+
+// Question 4
+// var word = "Hello World"
+// document.write("String: "+word +"<br><br>")
+// document.write("Last index of 'l': "+word.lastIndexOf("l"))
+
+// Question 5
+// var word = "Pakistani"
+// document.write("String: "+word +"<br><br>")
+// document.write("Character at index 3: "+ word.charAt("3"))
+
+// Chapter 26 to 34
+
+// Question 1
+// var userNumber = +prompt("Enter any number")
+// document.write("number: "+Number(userNumber)+"<br>")
+// document.write("round off value: "+Math.round(userNumber)+"<br>")
+// document.write("floor value: "+Math.floor(userNumber)+"<br>")
+// document.write("ceil value: "+Math.ceil(userNumber)+"<br>")
+
+// Question 2
+// var userNumber = +prompt("Enter any number")
+// document.write("number: "+Number(userNumber)+"<br>")
+// document.write("round off value: "+Math.round(userNumber)+"<br>")
+// document.write("floor value: "+Math.floor(userNumber)+"<br>")
+// document.write("ceil value: "+Math.ceil(userNumber)+"<br>")
+
+// Question 3
+// var userNumber = +prompt("Enter any number")
+// document.write("number: "+Number(userNumber)+"<br>")
+// document.write("round off value: "+Math.round(userNumber)+"<br>")
+// document.write("floor value: "+Math.floor(userNumber)+"<br>")
+// document.write("ceil value: "+Math.ceil(userNumber)+"<br>")
+
+// Question 4
+// var userNumber = +prompt("Enter any number")
+// document.write("number: "+Number(userNumber)+"<br>")
+// document.write("round off value: "+Math.round(userNumber)+"<br>")
+// document.write("floor value: "+Math.floor(userNumber)+"<br>")
+// document.write("ceil value: "+Math.ceil(userNumber)+"<br>")
+
